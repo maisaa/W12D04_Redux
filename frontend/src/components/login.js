48d6f5322a22334c1b281c22b8d8bdff03fa6e23
maisaa <maisaa.alkhedr@gmail.com>
2021-06-24 10:19:03 +0200

    login component with state

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,24 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { LoginContext } from './../context/login';
+// import { LoginContext } from './../context/login';
+import { useDispatch, useSelector } from 'react-redux';
+import { setToken } from '../reducer/login';
+import axios from 'axios';
 
 const Login = () => {
-	const loginContext = useContext(LoginContext);
 	const history = useHistory();
+	const dispatch = useDispatch();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		loginContext.login();
-		if (loginContext.loggedIn) {
-			history.push('/dashboard');
-		}
+	const state = useSelector((state) => {
+		// specify which state to subscribe to (state tree => reducer => state name )
+		return {
+			token: state.loginReducers.token,
+		};
+	});
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [message, setMessage] = useState('');
+
+	// function saveToken(token) {
+	// 	const user = jwt.decode(token);
+	// 	if (user) {
+	// 		dispatch(setToken(token));
+	// 		setUserId(user.userId);
+	// 		localStorage.setItem('token', token);
+	// 	}
+	// }
+
+	const handleSubmit = () => {
+
+		axios.post('http://localhost:5000/login', {
+			email,
+			password,
+		})
+			.then((result)=>{
+                // saveToken(result.data.token);
+				dispatch(setToken(result.data.token));
+				history.push('/dashboard');
+			})
+			.catch((err) => {
+				setMessage(err.response.data);
+			})
+		
 	};
 
-	const redirect = () => {
-		if (loginContext.loggedIn) {
-			history.push('/dashboard');
-		}
-	};
 
 	return (
 		<>
@@ -26,18 +53,16 @@ const Login = () => {
 				<input
 					type="email"
 					placeholder="email here"
-					onChange={(e) => loginContext.setEmail(e.target.value)}
+					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<input
 					type="password"
 					placeholder="password here"
-					onChange={(e) => loginContext.setPassword(e.target.value)}
+					onChange={(e) => setPassword(e.target.value)}
 				/>
 				<button>Login</button>
 			</form>
-
-			{redirect()}
-			{loginContext.message && <div>{loginContext.message}</div>}
+			{message && <div>{message}</div>}
 		</>
 	);
 };
