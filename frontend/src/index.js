c1bba26d18ef5d93ca9ceac3d685157ad4fd10cc
maisaa <maisaa.alkhedr@gmail.com>
2021-06-24 09:08:20 +0200

    add redux provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,22 +4,31 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 //context providers
-import RegisterProvider from './context/register';
-import LoginProvider from './context/login';
-import NewArticleProvider from './context/newArticle';
-import DashboardProvider from './context/dashboard';
+import store from './reducer';
+import Provider from 'react-redux';
+// import RegisterProvider from './context/register';
+// import LoginProvider from './context/login';
+// import NewArticleProvider from './context/newArticle';
+// import DashboardProvider from './context/dashboard';
 
 ReactDOM.render(
+
 	<Router>
-		<LoginProvider>
-			<RegisterProvider>
-				<NewArticleProvider>
-					<DashboardProvider>
-						<App />
-					</DashboardProvider>
-				</NewArticleProvider>
-			</RegisterProvider>
-		</LoginProvider>
-	</Router>,
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</Router>
+	,
+	// <Router>
+	// 	<LoginProvider>
+	// 		<RegisterProvider>
+	// 			<NewArticleProvider>
+	// 				<DashboardProvider>
+	// 					<App />
+	// 				</DashboardProvider>
+	// 			</NewArticleProvider>
+	// 		</RegisterProvider>
+	// 	</LoginProvider>
+	// </Router>,
 	document.getElementById('root'),
 );
